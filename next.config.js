8a943ca13d27319a13fa68de88dcee08088812fc
Larry Saytee <saytee.software@web-developer.one>
2025-07-04 17:48:53 +0000

    make the Paypal payment option work in the local environment and on Netl

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_PAYPAL_CLIENT_ID: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+  },
   images: {
     remotePatterns: [
       {
